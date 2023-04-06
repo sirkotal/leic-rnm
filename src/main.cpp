3878004d24c1b69e3025794a59f608735cab515a
Mariana <73724268+golangis@users.noreply.github.com>
2023-04-07 00:53:45 +0100

    there are some problems regarding case sensitivity

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 #include "manager.h"
-#include "station.h"
-#include "vertexedge.h"
-#include "graph.h"
 
 void invalidInput(){
     std::cout << '\n';
@@ -19,19 +16,31 @@ int main() {
                    "----------------------------------------------------------\n"
                    "~          Welcome to the Fast Railway Service!          ~\n"
                    "----------------------------------------------------------\n"
-                   "How can we help you today?\nInsert the number of the operation.\n"
-                   "------------------------------------------------------------------------------------------------\n"
-                   "| 1 - Calculate no of trains that can simultaneously travel between two specific stations.      |\n"
-                   "| 2 - Determine no of trains that can arrive together at a specific station.                    |\n"
-                   "| 3 - The same as 2, but considering failures on the route.                                     |\n"
-                   "| 4 - Compute pairs of stations that need more trains between them.                             |\n"
-                   "| 5 - Estimate which stations need more maintenance.                                            |\n"
-                   "| 6 - Evaluate which stations are the most affected by failures on a route.     (TODO)          |\n"
-                   "| 0 - Exit Program                                                                              |\n"
-                   "------------------------------------------------------------------------------------------------\n"
+                   "How can we help you today?\nInsert the number of the operation (1-7).\n"
+                   "-------------------------------------------------------------------------------------------------------\n"
+                   "|    BASIC SERVICES                                                                                   |\n"
+                   "| 1 - (2.1) Calculate max no of trains that can simultaneously travel between two specific stations.  |\n"
+                   "| 2 - (2.2) Compute pairs of stations that need more trains between them.                             |\n"
+                   "| 3 - (2.3) Estimate which stations need more budget and maintenance.                                 |\n"
+                   "| 4 - (2.4) Determine no of trains that can arrive together at a specific station.                    |\n"
+                   "|                                                                                                     |\n"
+                   "|    COST OPTIMIZATION                                                                                |\n"
+                   "| 5 - (3.1) Calculate the max no of trains that can simultaneously travel between                     |\n"
+                   "|           two specific stations with minimum cost for the company                                   |\n"
+                   "|                                                                                                     |\n"
+                   "|    RELIABILITY AND SENSITIVITY TO LINE FAILURES                                                     |\n"
+                   "| 6 - (4.1) Calculate the max no of trains that can simultaneously travel between                     |\n"
+                   "|           two specific stations in a network of reduced connectivity.                               |\n"
+                   "| 7 - (4.2) Compute the stations more affected due to a segment failure.                              |\n"
+                   "|                                                                                                     |\n"
+                   "| 0 - Exit Program                                                                                    |\n"
+                   "-------------------------------------------------------------------------------------------------------\n"
                    "> ";
     std::cin >> choice;
+
     string src, dst;
+    int k;
+
     if (!std::cin.fail()){
         switch(choice){
             case '1':
@@ -41,11 +50,32 @@ int main() {
                 std::cout << "  Choose destination station.\n       >";
                 std::getline(std::cin, dst);
                 std::cout << "The maximum number of trains that can travel between " << src << " and " << dst << " is: ";
-                std::cout << manager.maxTrains(src, dst);
-
+                std::cout << manager.maxTrains(src, dst) << "\n";
                 break;
             case '2':
-                std::cout << "Implement opt 2\n";
+                break;
+            case '3':
+                break;
+            case '4':
+                std::cin.ignore(); //clear the buffer
+                std::cout << "  Choose destination station.\n       >";
+                std::getline(std::cin, dst);
+                std::cout << "The maximum number of trains that can arrive at " << dst << " simultaneously is: ";
+                std::cout << manager.maxArrivalTrainsAtCertain(dst) << "\n";
+                break;
+            case '5':
+                std::cin.ignore(); //clear the buffer
+                std::cout << "  Choose source station.\n        >";
+                std::getline(std::cin, src);
+                std::cout << "  Choose destination station.\n       >";
+                std::getline(std::cin, dst);
+                std::cout << "The maximum number of trains that can travel between " << src << " and " << dst << " with minimum cost to the company is: ";
+                std::cout << manager.maxTrainsMinCost(src, dst) << "\n";
+                break;
+                break;
+            case '6':
+                break;
+            case '7':
                 break;
             case '0':
                 std::cout << "So sorry to see you go! :(\n";

ab83c7a4382cc539d4bda639ee05f8ece8ff8bf2
sirkotal <lordkotal2002@gmail.com>
2023-04-07 20:29:14 +0100

    Finished the Vertex class's documentation

--- headers/vertexedge.h
+++ headers/vertexedge.h
@@ -19,26 +19,110 @@ class Vertex {
 public:
     Vertex(const Station &station);
 
+    /**
+     * @brief Gets the vertex's adjacency list
+     * @return A vector with all of the vertex's outgoing edges
+     */
     std::vector<Edge *> getAdj() const;
+
+    /**
+     * @brief Gets the train station associated to the vertex
+     * @return The vertex's station
+     */
     Station getStation() const;
+
+    /**
+     * @brief Checks if the vertex has been visited
+     * @return True if the vertex has been visited; if it hasn't, it returns false
+     */
     bool isVisited() const;
+
+    /**
+     * @brief Checks if the vertex is being processed
+     * @return True if the vertex is being processed; if it isn't, it returns false
+     */
     bool isProcessing() const;
+
+    /**
+     * @brief Gets the vertex's indegree
+     * @return The indegree of the vertex
+     */
     unsigned int getIndegree() const;
+
+    /**
+     * @brief Gets the path that travels to the vertex
+     * @return The edge that travels to the vertex
+     */
     Edge *getPath() const;
+
+    /**
+     * @brief Gets all of the vertex's incoming edges
+     * @return A vector with all of the vertex's incoming edges
+     */
     std::vector<Edge *> getIncoming() const;
+
+    /**
+     * @brief Gets the vertex's distance (cost) to a source vertex
+     * @return The vertex's distance to the source
+     */
     int getDistance() const;
 
+    /**
+     * @brief Sets the vertex to either visited or unvisited
+     * @param visited True if setting the vertex to visited; otherwise, it is false
+     */
     void setVisited(bool visited);
+
+    /**
+     * @brief Sets the vertex to either being processed or not
+     * @param processing True if setting the vertex to being processed; otherwise, it is false
+     */
     void setProcesssing(bool processing);
+
+    /**
+     * @brief Sets the vertex's indegree
+     * @param indegree The vertex's new indegree
+     */
     void setIndegree(unsigned int indegree);
+
+    /**
+     * @brief Sets the path to the vertex
+     * @param path The edge that leads to the vertex
+     */
     void setPath(Edge *path);
+
+    /**
+     * @brief Sets the vertex's distance (cost) to a source vertex
+     * @param num The vertex's distance to the source
+     */
     void setDistance(int num);
-    Edge * addEdge(Vertex *dest, double w, const string &service);
+
+    /**
+     * @brief Adds an edge that originates from the vertex
+     * @param dest The destination vertex
+     * @param w The edge's capacity/weight
+     * @param service The edge's type of service
+     * @return The new edge that has been added
+     */
+    Edge* addEdge(Vertex *dest, double w, const string &service);
+
+    /**
+     * @brief Removes an edge that originates from the vertex
+     * @param destID The destination vertex's station name
+     * @return True if the edge was successfully removed; if it doesn't exist, it returns false
+     */
     bool removeEdge(string destID);
 
-    bool operator()(const Vertex* s, const Vertex* t) const;
-
+    /**
+     * @brief Gets the previous vertex
+     * @return The previous vertex
+     */
     Vertex* getSRC();
+
+    /**
+     * @brief Sets the previous vertex
+     * @param v The previous vertex
+     */
     void setSRC(Vertex *v);
 
 private:
